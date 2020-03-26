a482084c0bd6a219f783381e9c3f51f8b7c62f85
zamoroz <moras8465132@gmail.com>
2020-03-26 16:47:08 +0700

    В MainActivity в методе onCreate свяжите соответствующие переменные с идентификаторами полей в макете

--- app/src/main/java/com/example/lr1/MainActivity.java
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = editText.findViewById(R.id.editText);
+        textView = textView.findViewById(R.id.textView3);
+
     }
 
 }
