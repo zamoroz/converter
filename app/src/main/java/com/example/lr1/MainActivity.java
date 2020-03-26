129f701f17f4e051b81b7b36a2cff9b92abd9bb1
zamoroz <moras8465132@gmail.com>
2020-03-26 16:47:56 +0700

    Создайте метод onButtonClick, который будет обрабатывать нажатие кнопки

--- app/src/main/java/com/example/lr1/MainActivity.java
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lr1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         editText = editText.findViewById(R.id.editText);
         textView = textView.findViewById(R.id.textView3);
 
+    }
+
+    public void onButtonClick(View v) {
+        liters = Double.parseDouble(editText.getText().toString());
+        gallons = liters*0.264172;
+        textView.setText(String.valueOf(gallons));
     }
 
 }
